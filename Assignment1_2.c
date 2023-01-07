f4bcea2d12af20760ce022082bfe6988723a0bc1
Priyanka Salunkhe <114103435+PRIYANKA-SALUNKHE9@users.noreply.github.com>
2023-01-07 12:04:37 +0530

    print 5 times "Hello" on screen

---
+++ Assignment1_2.c
@@ -0,0 +1,30 @@
+
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Display()
+// Description   :  print 5 times "Hello" on screen
+// Input         :  string
+// Output        :  string
+// Author        :  Priyanka Salunkhe
+// Date          :  21 Apr 2022
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Display()
+{
+	int i=0;
+	for(i=1;i<=5;i++)
+	{
+		printf("Hello\n");
+	}
+}
+
+int main()
+{
+	Display();
+	
+	return 0;
+}
