1997c38b5f16ce3295b1ce53a5813e77d36c8cf5
Priyanka Salunkhe <114103435+PRIYANKA-SALUNKHE9@users.noreply.github.com>
2023-12-01 16:55:51 +0530

    Accept on number from user if number is less than 10 then print "Hello" otherwise print "Demo"

---
+++ Assignment2_3.c
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Function Name  :  Display()
+// Description   :  Accept on number from user if number is less than 10 then print "Hello" otherwise print "Demo" 
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Priyanka Salunkhe
+// Date          :  21 Apr 2022
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+	if(iNo<10)
+	{
+		printf("Hello");
+	}
+	else
+	{
+		printf("Demo");
+	}
+}
+
+int main()
+{
+	int iValue=0;
+	
+	printf("Enter number\n");
+	scanf("%d",&iValue);
+	
+	Display(iValue);
+	
+	return 0;
+}
