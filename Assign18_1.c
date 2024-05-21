0802e0cd4a72f83eccb90e23379f44ab5fa11ae1
Priyanka Salunkhe <114103435+PRIYANKA-SALUNKHE9@users.noreply.github.com>
2024-05-21 17:42:58 +0530

    Accept number of rows and number of column from user and display pattern

---
+++ Assign18_1.c
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()
+// Description   :  Accept number of rows and number of column from user and display pattern 
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Priyanka Salunkhe
+// Date          :  16 May 2022
+//
+////////////////////////////////////////////////////////////////////////////
+
+/*
+	Input:	iRow=4	iCol=4
+	Output:		*	*	*	#
+				*	*	#	*
+				*	#	*	*
+				#	*	*	*
+*/
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+	int iCnt1=0,iCnt2=0;
+	
+	for(iCnt1=1;iCnt1<=iRow;iCnt1++)
+	{
+		for(iCnt2=iCol;iCnt2>=1;iCnt2--)
+		{
+			if(iCnt1==iCnt2)
+			{
+				printf("#\t");
+			}
+			else
+			{
+				printf("*\t");
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int iValue1=0,iValue2=0;
+	
+	printf("Enter number of rows and column \n");
+	scanf("%d %d",&iValue1,&iValue2);
+	
+	Pattern(iValue1,iValue2);
+	
+	return 0;
+}
